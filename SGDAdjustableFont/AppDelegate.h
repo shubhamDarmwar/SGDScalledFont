32a1fb14502f0d2a0396a36d76232f2b80fe7fa8
@Laxman Murugappan <mlaxman.m@gmail.com>
2016-06-13 12:19:24 +0530

    Initial Commit

---
+++ SGDAdjustableFont/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SGDAdjustableFont
+//
+//  Created by Laxman Murugappan on 6/13/16.
+//  Copyright © 2016 Shubham Daramwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SGDAdjustableFont/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SGDAdjustableFont
+//
+//  Created by Laxman Murugappan on 6/13/16.
+//  Copyright © 2016 Shubham Daramwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
